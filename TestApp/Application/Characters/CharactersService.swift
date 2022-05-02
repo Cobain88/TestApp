35b70c5d929671c813fa80d8dc530b57c0c1d031
Dario Abete <darioabeteinge88@gmail.com>
2022-05-02 13:55:13 +0200

    GraphQL setup

---
+++ TestApp/Application/Characters/CharactersService.swift
@@ -0,0 +1,17 @@
+//
+//  CharactersService.swift
+//  TestApp
+//
+//  Created by Dario Abete on 02/05/22.
+//
+
+import Foundation
+import Apollo
+
+class CharactersService {
+
+  static let shared = CharactersService()
+
+  private(set) lazy var apollo = ApolloClient(url: URL(string: "https://rickandmortyapi.com/graphql")!)
+
+}
